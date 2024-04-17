73548fc79ed83bee144ab878546038bfe9196d9f
W1ndEee <danchacha111@gmail.com>
2024-04-17 17:05:42 +0800

    first commit

---
+++ register-commands.js
@@ -0,0 +1,54 @@
+const {REST, Routes, ApplicationCommandOptionType} = require('discord.js');
+const {token, guild_id, bot_id} = require("./config.json");
+
+const commands = [
+    {
+        name: 'hey',
+        description: 'Replies with hey!',
+    },
+    {
+        name: 'ping',
+        description: 'Pong!',
+    },
+    {
+        name: 'add',
+        description: 'Adds two numbers.',
+        options: [
+            {
+                name: 'first-number',
+                description: 'The first number',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            },
+            {
+                name: 'second-number',
+                description: 'The second number',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            },
+        ]
+    },
+    {
+        name: 'embed',
+        description: 'Sends an embed!',
+    },
+];
+
+const rest = new REST({version: '10'}).setToken(token);
+
+(async () => {
+    try {
+        console.log('Registering slash commands....');
+
+
+        await rest.put(
+            Routes.applicationGuildCommands(bot_id, guild_id),
+            { body: commands}
+        )
+
+        console.log('Commands were registered successfully!');
+    }
+    catch (error) {
+        console.log(error);
+    }
+})();
---
+++ src/index.js
@@ -0,0 +1,68 @@
+const {Client, Events, GatewayIntentBits, IntentsBitField, EmbedBuilder, ActivityType} = require("discord.js");
+const {token, guild_id, bot_id} = require("./config.json");
+const eventHandler = require("./handlers/eventHandler");
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+eventHandler(client);
+/*
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online.`);
+
+    client.user.setActivity({
+        name: "Having fun!",
+        type: ActivityType.Playing,
+    });
+});
+
+client.on('messageCreate', (message) => {
+
+    console.log(message.content);
+
+    if (message.author.bot) {
+        return;
+    }
+
+    //To @ Someone, use <@${message.author.id}>, to @everyone just use @everyone. To @ a role, use <@&${id(for the role)}>
+    if (message.content === 'hello <@' + bot_id + '>') {
+        message.reply(`Hey! <@${message.author.id}>`);
+    }
+});
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    
+    if (interaction.commandName === 'hey') {
+        interaction.reply('Hey!');
+    }
+
+    if (interaction.commandName === 'ping') {
+        interaction.reply('Pong!');
+    }
+
+    if (interaction.commandName === 'add') {
+        const num1 = interaction.options.get('first-number').value;
+        const num2 = interaction.options.get('second-number').value;
+
+        interaction.reply(`The sum is ${num1 + num2}`);
+    }
+
+    if (interaction.commandName === 'embed') {
+        const embed = new EmbedBuilder()
+        .setTitle("Embed title.")
+        .setDescription("This is a description!")
+        .setColor('Random')
+        .addFields({name: 'Field title', value: 'Some random value', inline: true});
+
+        interaction.reply({embeds: [embed]});
+    }
+});
+*/
+client.login(token);
